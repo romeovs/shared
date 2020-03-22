fa63c0c8e58857d40a6822e84576d4e67964748d
Romeo <romeovs@gmail.com>
2020-03-22 12:08:15 +0100

    Remove spaces in function definitions

--- template/src/frame.js
+++ template/src/frame.js
@@ -1,7 +1,7 @@
 import { Route, type RouteDef } from "@romeovs/frame/server"
 
 export default {
-	async routes (): Promise<RouteDef<mixed>[]> {
+	async routes(): Promise<RouteDef<mixed>[]> {
 		return [
 			Route("/", import("./pages/index"), {}),
 		]
